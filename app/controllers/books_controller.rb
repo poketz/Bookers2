271f6fb1077e8ad6ebefadc721ccb83897bf4be5
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2022-11-11 14:44:26 +0000

    [Add] create機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,29 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(:id)
+  end
+  
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get '/homes/about' => 'homes#about', as: 'about'
-  resources :users, only: [:show, :edit]
-  resources :books, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :edit, :create, :destroy]
 end
