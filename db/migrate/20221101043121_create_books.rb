a96ca03d8792877f15412109b4b5cdca6ec7b351
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2022-11-21 04:56:43 +0000

    [Fix] カラムの重複を修正

--- db/migrate/20221101043121_create_books.rb
+++ db/migrate/20221101043121_create_books.rb
@@ -1,10 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-      t.integer :user_id
       t.string :title
       t.string :body
-
+      t.integer :user_id
       t.timestamps
     end
   end
